762c742825143f4a5f5119c7516ed640ebb9196b
bharathrames <bharath91505@gmail.com>
2023-08-01 15:20:21 +0530

    get

--- index.js
+++ index.js
@@ -27,6 +27,10 @@ databaseConnection()
 app.use(express.json())
 app.use(cors())
 
+app.get("/", function (req, res) {
+    res.send("<h1>urlshortner</h1>");
+  });
+
 // Routers
 // signup router
 app.use("/signup",signupRouter)
